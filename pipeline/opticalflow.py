7f5ef23b006ca18fcb33c395ccaa537c61a0731a
Julia <julia.moor@study.hs-duesseldorf.de>
2025-06-04 17:43:47 +0200

    optical flow preparation

--- pipeline/opticalflow.py
+++ pipeline/opticalflow.py
@@ -1,3 +1,6 @@
+import numpy as np
+import cv2
+
 class OpticalFlow:
     def __init__(self):
         self.name = "Optical Flow" # Do not change the name of the module as otherwise recording replay would break!
@@ -13,7 +16,15 @@ class OpticalFlow:
     def step(self, data):
         # TODO: Implement processing of a single frame
         # The task of the optical flow module is to determine the overall avergae pixel shift between this and the previous image. 
-        # You 
+        current_frame = data["image"]
+        previous_frame = None
+        motion_vector = {[0, 0]} 
+
+        if previous_frame is None:  # If there is no previous frame, there is no motion to calculate
+            previous_frame = current_frame
+            return {"opticalFlow": np.array([[0.0, 0.0]])}
+
+        # also eigentlich brauche ich noch die dinge die getrckt werden sollen vom Tracker deswegen idk
 
         # Note: You can access data["image"] to receive the current image
         # Return a dictionary with the motion vector between this and the last frame
@@ -22,4 +33,3 @@ class OpticalFlow:
         return {
            "opticalFlow": None
         }
-
