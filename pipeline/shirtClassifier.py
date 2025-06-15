ef5ec644753a98df447edb5f1e1da397d6634807
annesbl <annesoballa@icloud.com>
2025-06-15 16:23:36 +0200

    lol hab die ganze zeit im master gearbeitet und nicht in meinem branch

--- pipeline/shirtClassifier.py
+++ pipeline/shirtClassifier.py
@@ -1,17 +1,40 @@
+import numpy as np
+import cv2
+
 class ShirtClassifier:
     def __init__(self):
         self.name = "Shirt Classifier" # Do not change the name of the module as otherwise recording replay would break!
+        self.teamA_color = None
+        self.teamB_color = None
+        self.initialized = False
 
     def start(self, data):
-        # TODO: Implement start up procedure of the module
-        pass
+        self.initialized = False 
+        #pass
 
     def stop(self, data):
         # TODO: Implement shut down procedure of the module
         pass
 
     def step(self, data):
-        # TODO: Implement processing of a current frame list
+        image = data["image"]
+        tracks = data["tracks"]
+        track_classes = data.get("trackClasses", [])
+
+        
+        
+        if len(tracks) == 0 or len(track_classes) == 0:
+                return {
+                  "teamAColor": (0, 0, 0),
+                  "teamBColor": (0, 0, 0),
+                  "teamClasses": []
+              }
+                
+                
+                
+                
+                
+# TODO: Implement processing of a current frame list
         # The task of the shirt classifier module is to identify the two teams based on their shirt color and to assign each player to one of the two teams
 
         # Note: You can access data["image"] and data["tracks"] to receive the current image as well as the current track list
@@ -22,6 +45,8 @@ class ShirtClassifier:
         #           0: Team not decided or not a player (e.g. ball, goal keeper, referee)
         #           1: Player belongs to team A
         #           2: Player belongs to team B
+        
         return { "teamAColor": None,
                  "teamBColor": None,
                  "teamClasses": None }
+        
